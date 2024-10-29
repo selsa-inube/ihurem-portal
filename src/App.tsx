78926aef405ab7654867656879b2ee300db2f8a9
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-10-29 12:45:06 -0500

    fix: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppPage } from "@components/layout/AppPage";
 import { AppProvider } from "./context/AppContext";
+import { GlobalStyles } from "./styles/global";
 
 function App() {
   return (
     <AppProvider>
       <Router>
+        <GlobalStyles />
         <AppPage />
       </Router>
     </AppProvider>
