fc45f300254de97af5bbb9fe7cb0753d2b2b8922
Diego Pinilla <dpinilla@sistemaenlinea.com.co>
2024-10-30 17:51:35 -0500

    feat: implement auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 import { AppPage } from "@components/layout/AppPage";
 import { AppProvider } from "./context/AppContext";
 import { GlobalStyles } from "./styles/global";
+import { enviroment } from "./config/environment";
+
+function LogOut() {
+  localStorage.clear();
+  const { logout } = useAuth0();
+  void logout({ logoutParams: { returnTo: enviroment.REDIRECT_URI } });
+  return <AppPage />;
+}
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<AppPage />}></Route>
+      <Route path="logout" element={<LogOut />} />
+    </>,
+  ),
+);
 
 function App() {
+  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      void loginWithRedirect();
+    }
+  }, [isLoading, isAuthenticated, loginWithRedirect]);
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
   return (
     <AppProvider>
-      <Router>
-        <GlobalStyles />
-        <AppPage />
-      </Router>
+      <GlobalStyles />
+      <RouterProvider router={router} />
     </AppProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+import { enviroment } from "./config/environment";
 
 import App from "./App";
+
+const redirect_uri = enviroment.REDIRECT_URI;
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={enviroment.AUTH0_DOMAIN}
+      clientId={enviroment.CLIENT_ID}
+      authorizationParams={{
+        redirect_uri,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 );
