3395c380d9b3598587a175384dfc8df098c0c661
Diego Pinilla <dpinilla@sistemaenlinea.com.co>
2024-10-30 10:04:48 -0500

    fix: component update

--- src/context/AppContext/index.tsx
+++ src/context/AppContext/index.tsx
@@ -7,28 +7,9 @@ import React, {
 } from "react";
 import selsaLogo from "@assets/images/selsa.png";
 import { useAuth0 } from "@auth0/auth0-react";
+import { IAppContextType, IPreferences } from "./types";
 
-interface Preferences {
-  boardOrientation: "vertical" | "horizontal";
-  showPinnedOnly: boolean;
-}
-
-interface AppContextType {
-  user: { username: string; id: string; company: string } | null;
-  setUser: React.Dispatch<
-    React.SetStateAction<{
-      username: string;
-      id: string;
-      company: string;
-    } | null>
-  >;
-  preferences: Preferences;
-  updatePreferences: (newPreferences: Partial<Preferences>) => void;
-  logoUrl: string;
-  setLogoUrl: React.Dispatch<React.SetStateAction<string>>;
-}
-
-const AppContext = createContext<AppContextType | undefined>(undefined);
+const AppContext = createContext<IAppContextType | undefined>(undefined);
 
 const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { user: auth0User } = useAuth0();
@@ -49,7 +30,7 @@ const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const initialLogo = localStorage.getItem("logoUrl") ?? selsaLogo;
   const [logoUrl, setLogoUrl] = useState<string>(initialLogo);
 
-  const [preferences, setPreferences] = useState<Preferences>({
+  const [preferences, setPreferences] = useState<IPreferences>({
     boardOrientation:
       (localStorage.getItem("boardOrientation") as "vertical" | "horizontal") ??
       "vertical",
@@ -57,7 +38,7 @@ const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
       JSON.parse(localStorage.getItem("showPinnedOnly") ?? "false") === true,
   });
 
-  const updatePreferences = (newPreferences: Partial<Preferences>) => {
+  const updatePreferences = (newPreferences: Partial<IPreferences>) => {
     setPreferences((prev) => ({ ...prev, ...newPreferences }));
   };
 
