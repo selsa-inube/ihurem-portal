fb246fd0d279be60b3a83b7109290ae8bb228243
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-11-27 18:28:21 -0500

    fix: component adjustment

---
+++ src/pages/certifications/NewCertification/index.tsx
@@ -0,0 +1,14 @@
+import { certificationsNavConfig } from "../config/nav.config";
+import { NewCertificationUI } from "./interface";
+
+function RequestEnjoyment() {
+  return (
+    <NewCertificationUI
+      appName={certificationsNavConfig[1].label}
+      appRoute={certificationsNavConfig[1].crumbs}
+      navigatePage={certificationsNavConfig[1].url}
+    />
+  );
+}
+
+export { RequestEnjoyment };
--- src/routes/certifications.tsx
+++ src/routes/certifications.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
 import { CertificationsOptions } from "@pages/certifications";
+import { RequestEnjoyment } from "@src/pages/certifications/NewCertification";
 
 function CertificationsRoutes() {
   return (
     <Routes>
       <Route path="/" element={<CertificationsOptions />} />
+      <Route path="/new-certification" element={<RequestEnjoyment />} />
     </Routes>
   );
 }
