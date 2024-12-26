3425e0917ac5b18935af00badfa99d5fbdf2cfe6
Angelbt12 <125980817+Angelbt12@users.noreply.github.com>
2024-12-26 10:19:21 -0500

    At/ihp28/vacation detail of vacation enjoyment component (#35)
    * feat: modal implementation
    
    * fix: component adjustment

---
+++ src/pages/holidays/components/HolidaysTable/Actions/index.tsx
@@ -0,0 +1,43 @@
+import { MdClose } from "react-icons/md";
+import { Stack } from "@inubekit/stack";
+import { Text } from "@inubekit/text";
+import { Icon } from "@inubekit/icon";
+
+import { StyledContainer, StyledLi, StyledUl, StyledActions } from "./styles";
+import { Actions } from "./config";
+
+interface ActionModalProps {
+  onClose: () => void;
+  onClickDetails?: () => void;
+  onClickEdit?: () => void;
+  onClickEliminate?: () => void;
+}
+
+export function ActionModal(props: ActionModalProps) {
+  const { onClose, onClickDetails, onClickEdit } = props;
+
+  const actionsLi = Actions(onClickDetails, onClickEdit);
+  return (
+    <StyledContainer>
+      <StyledActions>
+        <Stack padding="10px 15px" width="132px">
+          <Icon
+            icon={<MdClose />}
+            appearance="dark"
+            size="24px"
+            onClick={onClose}
+            cursorHover
+          />
+          <StyledUl>
+            {actionsLi.map((item, index) => (
+              <StyledLi key={index} onClick={item.onClick}>
+                <Icon icon={item.icon} appearance={item.appearance} />
+                <Text size="medium">{item.label}</Text>
+              </StyledLi>
+            ))}
+          </StyledUl>
+        </Stack>
+      </StyledActions>
+    </StyledContainer>
+  );
+}
