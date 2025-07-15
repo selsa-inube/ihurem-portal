dc589fb0d3db0dbc8f2656ad18e0da730f38f338
Johan Daniel Garcia Nova <87447257+novadeveloper14@users.noreply.github.com>
2025-07-15 11:34:50 -0500

    ihp183 Pagina de autoregistro (#72)
    * feat: self registration page
    
    * fix: implement logo of business unit
    
    * fix: adjust portalCode validation

---
+++ src/pages/self-registration/interface.tsx
@@ -0,0 +1,77 @@
+import { Grid, useMediaQueries, Stack, Text } from "@inubekit/inubekit";
+
+import selsa from "@assets/images/selsa.png";
+import teamSuccess from "@assets/images/teamSuccess.png";
+import { spacing } from "@design/tokens/spacing";
+import { useAppContext } from "@context/AppContext/useAppContext";
+
+import {
+  StyledWelcomeContainer,
+  StyledOutletContainer,
+  StyledImg,
+  StyledLogo,
+} from "./styles";
+import { RegistrationForm } from "./RegistrationForm";
+
+function SelfRegistrationUI() {
+  const { "(max-width: 835px)": screenMobile }: Record<string, boolean> =
+    useMediaQueries(["(max-width: 835px)"]);
+
+  const { logoUrl, businessUnit } = useAppContext();
+
+  const logoSrc = businessUnit?.urlLogo ?? logoUrl ?? selsa;
+  const logoAlt = businessUnit?.abbreviatedName ?? "logo selsa";
+
+  return (
+    <Grid
+      templateColumns={screenMobile ? "1fr" : "repeat(2, 1fr)"}
+      templateRows={screenMobile ? "minmax(360px, 47vh) 1fr" : "100vh"}
+    >
+      <StyledWelcomeContainer>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          height={screenMobile ? "auto" : "100%"}
+        >
+          <Stack
+            direction="column"
+            alignItems="center"
+            gap={screenMobile ? spacing.s250 : spacing.s400}
+          >
+            <StyledLogo src={logoSrc} alt={logoAlt} />
+            <Stack direction="column" alignItems="center">
+              <Text type="headline" size="small" weight="bold">
+                ¡Bienvenido!
+              </Text>
+              <Text
+                as="h1"
+                type="headline"
+                size={screenMobile ? "medium" : "large"}
+              >
+                Portal de empleados
+              </Text>
+            </Stack>
+            <StyledImg
+              src={teamSuccess}
+              $screenMobile={screenMobile}
+              alt="team success img"
+            />
+          </Stack>
+        </Stack>
+      </StyledWelcomeContainer>
+      <StyledOutletContainer>
+        <Stack
+          alignItems={screenMobile ? "flex-start" : "center"}
+          justifyContent="center"
+          height="-webkit-fill-available"
+          padding={`${spacing.s400} ${spacing.s300} ${spacing.s0}`}
+        >
+          <RegistrationForm />
+        </Stack>
+      </StyledOutletContainer>
+    </Grid>
+  );
+}
+
+export { SelfRegistrationUI };
