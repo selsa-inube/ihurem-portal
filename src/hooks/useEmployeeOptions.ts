4b3be1ecf0683283a0094ca28873952367d40fce
novadeveloper14 <johangarcianova14@gmail.com>
2025-10-03 03:36:54 -0500

    fix(useEmployeeOptions): adjust hook to sign out 1005

--- src/hooks/useEmployeeOptions.ts
+++ src/hooks/useEmployeeOptions.ts
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
+
 import { getEmployeeOptions } from "@services/employeePortal/getOptionsConsultation";
 import { IEmployeeOptions } from "@ptypes/employeePortalBusiness.types";
+
+import { useSignOut } from "./useSignOut";
 
 export const useEmployeeOptions = (employeeId: string) => {
   const [data, setData] = useState<IEmployeeOptions[] | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [codeError, setCodeError] = useState<number | undefined>(undefined);
+
+  const { signOut } = useSignOut();
 
   useEffect(() => {
     const fetchOptions = async () => {
@@ -17,6 +22,7 @@ export const useEmployeeOptions = (employeeId: string) => {
         const options = await getEmployeeOptions();
         if (options.length === 0) {
           setError("No existen opciones para el empleado");
+          signOut("/error?code=1005");
         }
         setData(options);
         setLoading(false);
