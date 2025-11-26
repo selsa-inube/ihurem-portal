ca4eb8b255e498e7e067c1a2d8d9ba8609a0c70c
novadeveloper14 <johangarcianova14@gmail.com>
2025-11-26 09:16:09 -0500

    fix: adjust redirect

--- src/pages/protectedRoutes/index.tsx
+++ src/pages/protectedRoutes/index.tsx
@@ -76,7 +76,11 @@ export function ProtectedRoutes() {
     if (portalData?.externalAuthenticationProvider !== undefined) {
       const externalIAuthProvider = portalData.externalAuthenticationProvider;
       if (!externalIAuthProvider) {
-        if (!isAuthenticated && !isLoading) {
+        if (
+          !isAuthenticated &&
+          !isLoading &&
+          window.location.pathname !== "/self-registration"
+        ) {
           loginWithRedirect({
             authorizationParams: {
               connection: "google-oauth2",
@@ -99,7 +103,8 @@ export function ProtectedRoutes() {
       !isAuthenticated &&
       !hasPortalError &&
       !pathStart.includes(window.location.pathname) &&
-      !portalData.externalAuthenticationProvider
+      !portalData.externalAuthenticationProvider &&
+      window.location.pathname !== "/self-registration"
     ) {
       loginWithRedirect({
         authorizationParams: {
