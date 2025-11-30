882adfa3c0f032755fb954562278e7d2e2e8bc44
Johan Daniel Garcia Nova <87447257+novadeveloper14@users.noreply.github.com>
2025-11-30 10:37:59 -0500

    ihp357 Implementar servicio POST para el auto-registro de empleados (#122)
    * feat: implement post service for employee self-registration
    
    * fix: redirect when close response modal
    
    * fix: adjust redirect
    
    * fix: adjust token requirement service
    
    * fix(RegistrationForm): adjust min max on idNumber field

---
+++ src/hooks/useSelfRegistrationAPI.ts
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+import { postSelfRegistration } from "@services/employeePortal/postSelfRegistration";
+import { ISelfRegistrationRequestBody } from "@services/employeePortal/postSelfRegistration/types";
+import { useErrorModal } from "@context/ErrorModalContext/ErrorModalContext";
+import { modalErrorConfig } from "@config/modalErrorConfig";
+
+const ERROR_CODE_POST_REGISTRATION_REQUESTS_FAILED = 1023;
+
+export function useSelfRegistrationAPI() {
+  const [humanResourceRequestId, setHumanResourceRequestId] = useState<
+    string | null
+  >(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { showErrorModal } = useErrorModal();
+  const [error, setError] = useState<Error | null>(null);
+
+  const submitSelfRegistration = async (
+    requestBody: ISelfRegistrationRequestBody,
+  ) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const headers = {
+        "Content-type": "application/json; charset=UTF-8",
+        "X-Business-unit": "test",
+      };
+      const response = await postSelfRegistration(requestBody, headers);
+
+      if (response?.humanResourceRequestId) {
+        setHumanResourceRequestId(response.humanResourceRequestId);
+        setIsLoading(false);
+        return { success: true, response };
+      }
+
+      setIsLoading(false);
+      return { success: false };
+    } catch (err) {
+      console.error("Error sending self-registration:", err);
+
+      const error = err instanceof Error ? err : new Error(String(err));
+      setError(error);
+
+      const errorConfig =
+        modalErrorConfig[ERROR_CODE_POST_REGISTRATION_REQUESTS_FAILED];
+      showErrorModal({
+        descriptionText: `${errorConfig.descriptionText}: ${String(err)}`,
+        solutionText: errorConfig.solutionText,
+      });
+
+      setIsLoading(false);
+      return { success: false };
+    }
+  };
+
+  return {
+    submitSelfRegistration,
+    humanResourceRequestId,
+    isLoading,
+    error,
+  };
+}
